dc42376fca9e1c51bd123df84ad8869b1fc443b8
charly <carlosgarita3110@gmail.com>
2022-11-09 11:38:26 -0600

    Class Definitio

---
+++ Hospital/ListaDeMedicos.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "NodoMedico.h"
+class ListaDeMedicos
+{
+private:
+	NodoMedico* head;
+public:
+	ListaDeMedicos(NodoMedico* = NULL);
+	virtual ~ListaDeMedicos();
+	//getters
+	NodoMedico* getHead();
+	//setters
+	void setHead(NodoMedico*);
+	//metodos
+};
+
