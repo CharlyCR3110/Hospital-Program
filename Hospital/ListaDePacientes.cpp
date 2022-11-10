2ec7b5816b4cf1b3d51b14c0ca7706b3c9b7e415
charly <carlosgarita3110@gmail.com>
2022-11-09 20:20:02 -0600

    Add: Method that adds a patient to the list

--- Hospital/ListaDePacientes.cpp
+++ Hospital/ListaDePacientes.cpp
@@ -31,3 +31,33 @@ bool ListaDePacientes::isEmpty()
 {
 	return head == NULL;
 }
+
+bool ListaDePacientes::insertarPaciente(Paciente* paciente)
+{
+	NodoPaciente* nuevo = new NodoPaciente(paciente);
+	if (isEmpty())
+	{
+		head = nuevo;
+		return true;
+	}
+	else
+	{
+		NodoPaciente* aux = head;
+		// se debe de comprobar aqui en caso de que solo exista un paciente registrado
+		if (aux->getPaciente()->getIdentificacion() == paciente->getIdentificacion())
+		{
+			return false;
+		}
+		while (aux->getSiguiente() != NULL)
+		{
+			if (aux->getPaciente()->getIdentificacion() == paciente->getIdentificacion())
+			{
+				return false;
+			}
+			aux = aux->getSiguiente();
+		}
+		aux->setSiguiente(nuevo);
+		return true;
+	}
+	return false;
+}
--- Hospital/ListaDePacientes.h
+++ Hospital/ListaDePacientes.h
@@ -13,5 +13,6 @@ public:
 	void setHead(NodoPaciente*);
 	//metodos
 	bool isEmpty();
+	bool insertarPaciente(Paciente*);
 };
 
