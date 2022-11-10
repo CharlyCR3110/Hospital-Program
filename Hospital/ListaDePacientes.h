9856213230ce0ed5a011a11a7e738c6da8c4982b
charly <carlosgarita3110@gmail.com>
2022-11-09 20:04:11 -0600

    Class Definition

---
+++ Hospital/ListaDePacientes.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "NodoPaciente.h"
+class ListaDePacientes
+{
+private:
+	NodoPaciente* head;
+public:
+	ListaDePacientes(NodoPaciente* = NULL);
+	virtual ~ListaDePacientes();
+	//getters
+	NodoPaciente* getHead();
+	//setters
+	void setHead(NodoPaciente*);
+	//metodos
+};
+
