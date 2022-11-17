fef702353d254a57786b2921955de7ca2f72a6b3
charly <carlosgarita3110@gmail.com>
2022-11-17 10:20:29 -0600

    Delete: Trash includes

--- Hospital/main.cpp
+++ Hospital/main.cpp
@@ -1,21 +1,4 @@
-#include "ConPropiedad.h"
-#include "Interino.h"
-#include "Profesor.h"
-#include "Estudiante.h"
-#include "EstNacional.h"
-#include "EstInternacional.h"
-#include "ListaDeMedicos.h"
-#include "SaludUni.h"
-#include "Taxi.h"
-#include "Ambulancia.h"
-#include "Utiles.h"
 #include "Controlador.h"
-
-
-//void pausarYLimpiar() {
-//	system("pause");
-//	system("cls");
-//}
 
 int main() {
 	Controlador controlador;
